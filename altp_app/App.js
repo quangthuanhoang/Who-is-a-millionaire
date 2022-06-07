88ad880989809fd73d73e708fb3b5c2194bb5081
quangthuan <hoangquangthuan952@gmail.com>
2022-06-08 01:34:57 +0700

    add react native code

---
+++ altp_app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View } from 'react-native';
+import HomeScreen from './src/HomeScreen';
+const App = () => {
+  return (
+  <View style={{flex: 1}}>
+<HomeScreen />
+  </View>
+  )
+}
+export default App;
